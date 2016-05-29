d800e16ab72770f7a704d5515dab17689755a386
Eric Watkins <eric@threehour.co>
2016-05-29 15:52:42 -0500

    test

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('default', function() {
+
+
+
+});
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
